e25d4c49e911b37ededc77ac23aa713737181853
Anshu Rai <47187040+Anshu-rai89@users.noreply.github.com>
2023-03-28 22:53:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+var nameElement = document.querySelector("#name");
+var ageElement = document.querySelector("#age");
+var btnElement = document.querSelector("#btn");
+
+function handleSubmit(e) {
+	alert("test");
+}
+
+btnElement.addEventListener('click', handleSubmit);
